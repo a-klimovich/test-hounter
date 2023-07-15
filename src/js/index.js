6f58a2402afa694764b02657152f35425c854fa9
a-klimovich <ar2r.klimovich@gmail.com>
2023-07-15 18:18:04 +0200

    add: header

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
 ;(function () {
-  
+  const dropdown = document.getElementsByClassName('dropdown')
+  const dropdownBtns = document.getElementsByClassName('dropdown-btn')
+
+  const closeDropdown = () => {
+    for (let i = 0; i < dropdown.length; i++) {
+      dropdown[i].classList.remove('active')
+    }
+  }
+
+  for (let i = 0; i < dropdownBtns.length; i++) {
+    dropdownBtns[i].addEventListener('click', function(event) {
+      const dropdownContent = this.parentElement
+
+      dropdownContent.getElementsByClassName('active').length
+        ? dropdownContent.classList.remove('active')
+        : dropdownContent.classList.add('active')
+
+      event.stopPropagation()
+    })
+  }
+
+  document.body.addEventListener('click', closeDropdown)
 })()
