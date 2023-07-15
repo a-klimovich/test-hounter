b969fce3bf86f9d14106b4be5ab800dbc40ceeb2
a-klimovich <ar2r.klimovich@gmail.com>
2023-07-15 15:01:16 +0200

    add: section featured house

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,37 @@
 ;(function () {
-  const swiper = new Swiper('.swiperFeaturedHouse', {
-    slidesPerView: '1',
-    spaceBetween: 30,
+  const $prevBtn = document.getElementById('swiperFeaturedHouseBtnPrev')
+  const $nextBtn = document.getElementById('swiperFeaturedHouseBtnNext')
+
+  const swiperFeatureHouses = new Swiper('.swipe-featured-house', {
+    slidesPerView: 'auto',
+    freeMode: true,
+    spaceBetween: 40,
+    allowTouchMove: false
   })
+
+  $prevBtn.addEventListener('click', () => {
+    swiperFeatureHouses.slidePrev()
+  })
+
+  $nextBtn.addEventListener('click', () => {
+    swiperFeatureHouses.slideNext()
+  })
+
+  swiperFeatureHouses.on('slideChange', (e) => {
+    console.log(e);
+    if (e.isBeginning) {
+      $nextBtn.classList.remove('button-disabled')
+      $prevBtn.classList.add('button-disabled')
+    } else {
+      $prevBtn.classList.remove('button-disabled')
+      $nextBtn.classList.add('button-disabled')
+    }
+  });
 
   const heroSectionSwiper = new Swiper('.swiper-hero-section', {
     slidesPerView: 'auto',
     spaceBetween: 16,
     freeMode: true,
+    grabCursor: true,
   })
 })()
