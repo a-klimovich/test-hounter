7d431740cd1361aef12961a3e8dc839ad96ae8b8
a-klimovich <ar2r.klimovich@gmail.com>
2023-07-15 12:58:58 +0200

    add: hero section desc

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,4 +3,10 @@
     slidesPerView: '1',
     spaceBetween: 30,
   })
+
+  const heroSectionSwiper = new Swiper('.swiper-hero-section', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    freeMode: true,
+  })
 })()
